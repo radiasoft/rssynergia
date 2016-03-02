0c9d438ca16f35ac763163df96a090d1e36e0de3
robnagler <github@q33.us>
2016-03-02 03:36:49 +0000

    need a test so pkdeploy/test passes

---
+++ tests/null_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+u"""Empty test to pacify py.test
+
+:copyright: Copyright (c) 2016 RadiaSoft LLC.  All Rights Reserved.
+:license: http://www.apache.org/licenses/LICENSE-2.0.html
+"""
+from __future__ import absolute_import, division, print_function
+
+import pytest
+
+def test_nothing():
+    pass
