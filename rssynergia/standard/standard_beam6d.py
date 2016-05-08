4d28c2623f4a89b3ee6d4d50751db6deefc8f174
bruhwiler@radiasoft.net <bruhwiler@radiasoft.net>
2016-05-08 12:47:37 -0600

    fixing import statement for remote use on Jupyter

--- rssynergia/standard/standard_beam6d.py
+++ rssynergia/standard/standard_beam6d.py
@@ -6,23 +6,21 @@ import matplotlib.pyplot as plt
 from mpi4py import MPI
 from scipy import constants
 
-from standard import standard_beam
-from base_diagnostics import read_bunch
-from base_diagnostics import workflow
-from base_diagnostics import latticework
-
-
-
+import rssynergia
+from rssynergia.standard import standard_beam
+from rssynergia.base_diagnostics import read_bunch
+from rssynergia.base_diagnostics import workflow
+from rssynergia.base_diagnostics import latticework
 
 def toyKVbeam6D(opts):
     '''Generate a costing KV beam with fixed Hamiltonian and returns the particle array.
-    
-    Coordinates are chosen with fixed random number generator seed, so that they should always 
+
+    Coordinates are chosen with fixed random number generator seed, so that they should always
     produce the same initial distribution for a given emittance.
-    
+
     Args:
         opts (object): Instance of the Synergia options class containing beam and macroparticle information
-    
+
     Returns:
         bunch (ndarray): NumPy array with bunch with values {x, px, y, py, cdt, z, ID} for each particle
     '''
@@ -36,7 +34,7 @@ def toyKVbeam6D(opts):
     #Assume Gaussian longitudinal profile - put bunch length in m
     sigmaz = opts.stdz
     num_macro_particles = opts.macro_particles
-    
+
 
     xOffset = 0. #m
     yOffset = 0. #m
@@ -58,7 +56,7 @@ def toyKVbeam6D(opts):
     #innerBunch = np.zeros(numMacroParticles)
 
     for index,emit in enumerate(opts.emits):
-        
+
         innerbunch = np.zeros(num_macro_particles) #bunch at this emittance
         transverse_emittance = emit
 
@@ -68,19 +66,19 @@ def toyKVbeam6D(opts):
             mybunchgenerator = standard_beam.StandardBeam() #fixed beta=1, betaprime=0
         #coords is an array of 4-vectors containing coordinate space information
         coords = mybunchgenerator.generatefixedbunch(transverse_emittance, num_macro_particles, opts.seed)
-        
-        
+
+
         for idx in range(len(coords)):
             pArray[idx] = random.gauss(0, dpop)
             cdtArray[idx] = random.gauss(0, sigmaz)
-            
+
             #assign unique index value to each particle
             ID = index*len(coords) + idx
-        
+
             coords[idx] = np.append(coords[idx],[cdtArray[idx],pArray[idx],ID])
-        
+
         bunch.append(coords)
-        
+
 
     tofile = None
     if tofile:
@@ -90,12 +88,12 @@ def toyKVbeam6D(opts):
             print ' !Warning -- '
             print 'File '+filename+' already exists. Renaming the old file to '+newfilename
             os.rename('./'+filename, './'+newfilename)
-    
+
         bunchfile = open(filename, 'w')
         for idx in range(0,num_macro_particles):
             ptclstring = str(bunch[idx][0])+' '+str(bunch[idx][1])+' '+str(bunch[idx][2])+' '+str(bunch[idx][3])+' '+str(cdtArray[idx])+' '+str(pArray[idx])+'\n'
             bunchfile.write(ptclstring)
 
         bunchfile.close()
-        
+
     return np.asarray(bunch)
